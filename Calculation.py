2be5ed8961b06aa6bb5f7490038529f179f87e0f
SUSMITAM144 <67949473+SUSMITAM144@users.noreply.github.com>
2025-04-11 12:49:30 +0530

    Add files via upload

---
+++ Calculation.py
@@ -0,0 +1,10 @@
+a = int(input('Enter first number : '))
+b = int(input('Enter second number : '))
+add = a +b
+sub = a - b
+multiply = a * b
+div = a / b
+print('Addition  :' , add)
+print('Subtraction   :' , sub)
+print('Multiplication  :' , multiply)
+print('Division  :' , div)

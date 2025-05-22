7fca92aa1a407570c72c1308219a5ecaebf9ec98
SUSMITAM144 <67949473+SUSMITAM144@users.noreply.github.com>
2025-05-22 10:20:34 +0530

    Add files via upload

---
+++ built/ass1part2.py
@@ -0,0 +1,7 @@
+'''Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.'''
+fname = input("Enter your first name")
+lname = input("Enter your last name")
+print("Hey "+fname+" "+lname+" welcome to my page")
